b8ba3ebf0a49606ce7da5e76d9af71bb779f072f
cn <cn100800@163.com>
2018-10-21 15:44:07 +0800

    version.tar

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-go install
+# go install
 
-if [[ -z $GOPATH ]]; then
-    echo "GOPATH is not exists"
-    exit
-fi
+# if [[ -z $GOPATH ]]; then
+#     echo "GOPATH is not exists"
+#     exit
+# fi
 
-${GOPATH}/bin/new
+# ${GOPATH}/bin/new
 
-VERSION=$(date +%Y.%m.%d)".release"
+SUFFIX=".release"
+
+OUTPUT_DIR="output"
+
+RELEASE_DIR="release"
+
+VERSION=$(date +%Y.%m.%d)${SUFFIX}
 
 OLD_VERSION=$(git tag |sort -Vr |head -1)
 
 sed -i '' "s/${VERSION}/${OLD_VERSION}/g" conf.go
+
+make build
+
+tar -cvzf $RELEASE_DIR/$VERSION.tar.gz -C $OUTPUT_DIR .
 
 git tag $VERSION
 
