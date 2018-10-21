933cab53e7223a2b81052b11d9c4aa5bb3fbe900
cn <cn100800@163.com>
2018-10-21 11:32:02 +0800

    version

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
 go install
+
 if [[ -z $GOPATH ]]; then
     echo "GOPATH is not exists"
     exit
 fi
+
 ${GOPATH}/bin/new
+
+VERSION=$(date +%Y.%m.%d)".release"
+
+OLD_VERSION=$(git tag |sort -Vr |head -1)
+
+sed -i '' "s/${VERSION}/${OLD_VERSION}/g" conf.go
+
+git tag $VERSION
+
+git push origin $VERSION
