329e26bef2cd2a2221baf5772fdca9a5f36505a3
cn <cn100800@163.com>
2018-10-28 22:38:58 +0800

    修改权限

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash set -o errexit
 # go mod vendor
-wget -cq $(curl -sSf https://api.github.com/repos/cn100800/news/releases | grep release.tar.gz | awk 'NR==2 {printf $2"\n"}' | tr -d \") -O - | tar -xzC /usr/local/bin/
+wget -cq $(curl -sSf https://api.github.com/repos/cn100800/news/releases | grep release.tar.gz | awk 'NR==2 {printf $2"\n"}' | tr -d \") -O - | sudo tar -xzC /usr/local/bin/
 
