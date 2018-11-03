40c0006891a95e803e1437eb44eb21ac9a191179
cn <cn100800@163.com>
2018-11-03 17:40:56 +0800

    install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
-#!/bin/bash set -o errexit
-# go mod vendor
-wget -cq $(curl -sSf https://api.github.com/repos/cn100800/news/releases | grep release.tar.xz | awk 'NR==2 {printf $2"\n"}' | tr -d \") -O - | sudo tar -xjC /usr/local/bin/
+#!/bin/bash
+set -o errexit
+#go mod vendor
+wget -cq "$(curl -sSf https://api.github.com/repos/cn100800/news/releases | grep release.tar.xz | awk 'NR==2 {printf $2"\n"}' | tr -d \")" -O - | sudo tar -xjC /usr/local/bin/
